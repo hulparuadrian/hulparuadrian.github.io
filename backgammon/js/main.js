42843b8fabf6260a4b7d5c77075158a3bb21a985
Hulparu Adrian <hulparuadrian@gmail.com>
2016-03-08 19:58:05 +0200

    Bkgmn v0.1
    Working on initializing the game.

--- backgammon/js/main.js
+++ backgammon/js/main.js
@@ -1,24 +1,39 @@
-function Board(){
-	this.boardState = "";
-}
-
-//board initializer - to run after dom ready - get everything setup for the game
-Board.prototype = {
-	constructor : Board,
+var Board = {
+	boardState : [ 
+		[2, 0], [0, 0], [0, 0], [0, 0], [0, 0], [5, 1],
+		[0, 0], [3, 1], [0, 0], [0, 0], [0, 0], [5, 0],
+		[5, 1], [0, 0], [0, 0], [0, 0], [3, 0], [0, 0],
+		[5, 0], [0, 0], [0, 0], [0, 0], [0, 0], [2, 1]
+		],
 	initialize : function () {
-		Board.prototype.populateBoard();
+		this.populateBoard();
 	},
-	populateBoard : function () {	
-		// define shit
+	populateBoard : function () {
+		// define lines
 		for (var i = 1; i <= 4; i++) {
 			var qdr = document.getElementsByClassName("qdr-"+i);
 			for ( j = 1; j <= 6; j++) {
 				qdr[0].innerHTML += '<div class="cl cl-' + ((i-1)*6+j) + '"></div>';
 			}
 		}
+
+		//put pieces in place
+
+		for (var i = 0; i < 24; i++) {
+			if ( this.boardState[i][0] ) {
+				var type = (this.boardState[i][1]) ? "white" : "black";
+				var cont = document.getElementsByClassName("cl-"+(i+1));
+				for ( var j = 0; j < this.boardState[i][0]; j++ ) {
+					cont[0].innerHTML += '<div class="'+ type +'"></div>';
+				}
+			}
+		}
+
 	}
 }
 
 //start game
-var game = new Board();
-ready(game.initialize);
+
+var game = Object.create(Board);
+ready(function () {game.initialize();} );
+//game.initialize();
